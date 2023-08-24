f280779f1e80c8547ae91390b2a8a6900fa28c27
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-08-24 19:03:21 +0700

    CURD API

--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,12 @@ let data = [
 
 // Read list product
 router.get('/list', function(req, res, next) {
-  res.send(data);
+  try {
+    return res.send(data);
+  } catch (error) {
+    console.log('««««« error »»»»»', error);
+    return res.send(400, { message: "Không thành công" });
+  }
 });
 
 // Read list product
@@ -33,6 +38,87 @@ router.get('/:id', function(req, res, next) {
     {
       message: "Lấy thông tin thành công",
       payload: detail,
+    },
+  );
+});
+
+router.post('/', function(req, res, next) {
+  const { id, name, price } = req.body;
+
+  data = [
+    ...data,
+    { id, name, price },
+  ];
+
+  return res.send(
+    202,
+    {
+      message: "Tạo sản phẩm thành công",
+      payload: { id, name, price },
+    },
+  );
+});
+
+router.put('/:id', function(req, res, next) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  data = data.map((item) => {
+    if (item.id === +id) {
+      return {
+        id: item.id,
+        name,
+        price,
+      };
+    }
+
+    return item;
+  })
+
+  return res.send(
+    202,
+    {
+      message: "Cập nhật sản phẩm thành công",
+      payload: data,
+    },
+  );
+});
+
+router.patch('/:id', function(req, res, next) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  data = data.map((item) => {
+    if (item.id === +id) {
+      return {
+        ...item,
+        name: name || item.name,
+        price: price || item.price,
+      };
+    }
+
+    return item;
+  })
+
+  return res.send(
+    202,
+    {
+      message: "Cập nhật sản phẩm thành công",
+      payload: data,
+    },
+  );
+});
+
+router.delete('/:id', function(req, res, next) {
+  const { id } = req.params;
+
+  data = data.filter((item) => item.id !== +id)
+
+  return res.send(
+    202,
+    {
+      message: "Xóa sản phẩm thành công",
+      payload: data,
     },
   );
 });
